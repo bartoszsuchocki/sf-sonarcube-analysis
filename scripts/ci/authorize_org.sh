c63a5e30a3e0255706b0c36d51f0a86f6dc9259f
bartoszsuchocki <suchar1270@wp.pl>
2022-07-05 22:48:33 +0200

    Test jwt

--- scripts/ci/authorize_org.sh
+++ scripts/ci/authorize_org.sh
@@ -11,18 +11,6 @@ private_key=$2
 username=$3
 org_alias=$4
 
-echo "This is your client id: "
-echo $client_id
-
-echo "This is your username: "
-echo $username
-
-echo "This is your alias: "
-echo $org_alias
-
-echo "This is your private key: "
-echo $private_key
-
 mkdir auth_files
-echo $private_key > auth_files/private_key.key
+echo $private_key | base64 -d > auth_files/private_key.key
 sfdx auth:jwt:grant --clientid $client_id --jwtkeyfile auth_files/private_key.key --username $username --setalias $org_alias
