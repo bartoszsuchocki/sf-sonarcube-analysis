4ca3fc5be6e3093b84b3652832edc3c76ba82b59
bartoszsuchocki <suchar1270@wp.pl>
2022-07-05 23:13:28 +0200

    Test jwt

--- scripts/ci/authorize_org.sh
+++ scripts/ci/authorize_org.sh
@@ -1,7 +1,7 @@
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
     then 
         echo $#
-        echo 'Connected app client id, certificate private key, integration username and desired org alias should be provided'
+        echo 'Connected app client id, certificate private key, integration username, desired org alias and login instance url should be provided'
         exit 1
 fi
 
@@ -10,7 +10,8 @@ client_id=$1
 private_key=$2
 username=$3
 org_alias=$4
+instance_url=$5
 
 mkdir auth_files
 echo $private_key | base64 -d > auth_files/private_key.key
-sfdx auth:jwt:grant --clientid $client_id --jwtkeyfile auth_files/private_key.key --username $username --setalias $org_alias
+sfdx auth:jwt:grant --clientid $client_id --jwtkeyfile auth_files/private_key.key --username $username --setalias $org_alias --instance_url=$instance_url
